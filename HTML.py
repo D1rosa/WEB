080fdb509ca84f58250ec05b2de0b81088ed9c44
D1rosa <151547045+D1rosa@users.noreply.github.com>
2024-02-05 23:56:39 +0400

    Add files via upload

--- HTML.py
+++ HTML.py
@@ -249,5 +249,77 @@ def results(nickname, level, rating):
     """
 
 
+@app.route('/load_photo', methods=['POST', 'GET'])
+def photo():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}"/>
+                            <title>Отбор астронавтов</title>
+                          </head>
+                          <body>
+                            <h1 align="center">Загрузка фотографии</h1>
+                            <h2 align="center">для участии в миссии</h2>
+                            <div>
+                                <form>
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+                                    <img src="{url_for('static', filename='img/MARS.png')}" alt="">
+                                    <button type="submit" class="btn btn-primary">Отправить</button>
+                                </form>
+                            </div>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        with open('photo.jpeg', 'w') as img:
+            with open(request.form['file'], 'r') as file:
+                img.write(file)
+
+        return "Форма отправлена"
+
+
+@app.route('/carousel', methods=['POST', 'GET'])
+def carousel():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}"/>
+                            <title>Пейзажи Марса</title>
+                          </head>
+                          <body>
+                            <h1 align="center">Пейзажи Марса</h1>
+                                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                                  <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                      <img src="{url_for('static', filename='img/MARS1.jpg')}" class="d-block w-100" alt="...">
+                                    </div>
+                                    <div class="carousel-item">
+                                      <img src="{url_for('static', filename='img/MARS2.jpg')}" class="d-block w-100" alt="...">
+                                    </div>
+                                    <div class="carousel-item">
+                                      <img src="{url_for('static', filename='img/MARS3.jpg')}" class="d-block w-100" alt="...">
+                                    </div>
+                                  </div>
+                                </div>
+                          </body>
+                        </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
