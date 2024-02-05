853abbcc1fae75fba3031d52afb2a19f21b880fb
D1rosa <151547045+D1rosa@users.noreply.github.com>
2024-02-05 23:18:50 +0400

    Add files via upload

--- HTML.py
+++ HTML.py
@@ -186,5 +186,39 @@ def form_sample():
         return "Форма отправлена"
 
 
+@app.route('/choice/<planet_name>')
+def plan(planet_name):
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Варианты выбора</title>
+                  </head>
+                  <body>
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <h1>Моё предложение: {planet_name}</h1>
+                    <h2>Эта планета близка к Земле;</h2>
+                    <div class="alert alert-primary" role="alert">
+                      На ней много необходимых ресурсов;
+                    </div>
+                    <div class="alert alert-info" role="alert">
+                      На ней есть вода и атмосфера;
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      На ней есть небольшое магнитное поле;
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      Наконец, она просто красива!
+                    </div>
+                  </body>
+                </html>
+    """
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
